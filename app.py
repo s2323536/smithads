bf7c52865cd36c3a003d2b1a8c73a652463e0995
smith <103059412+s2323536@users.noreply.github.com>
2023-04-04 22:14:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,87 +1,53 @@
 from flask import Flask,request,render_template,jsonify,session
-import pandas as pd
-import googlemaps
-import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 
 app = Flask(
     __name__,
     static_folder='static',
     static_url_path='/'
 )
-app.secret_key="any string but secret"
-apiKey='AIzaSyD16pHbSZFQnQzL2HtWdsai4QOzr0bWLGU'
-gmaps = googlemaps.Client(key=apiKey)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/donate')
-def link():
-    return render_template('donate.html')
+@app.route('/index', methods=['POST'])
+def index():
+    name = request.form['name']
+    phone = request.form['phone']
+    email = request.form['email']
+    message_text = request.form['message']
+    image = request.files['image'] # 使用 request.files 取得上傳的文件
+     # 設置郵件內容
+    message = MIMEMultipart()
+    message['Subject'] = '玻璃訂製規格評估報價__'+ name
+    #message['From'] = 'rueishian.chen@gmail.com'
+    message['From'] = email
+    message['To'] = 'rueishian.chen@gmail.com'
 
-@app.route('/contact')
-def link2():
-    return render_template('contact.html')
+    # 添加文字內容
+    text = MIMEText(f"Name: {name}\nPhone: {phone}\nEmail: {email}\nmessage:{message_text}")
+    message.attach(text)
 
-@app.route('/flowchart')
-def link3():
-    return render_template('smithmap.html')
+    # 添加圖片內容
+    img = MIMEImage(image.read(), name=image.filename)
+    message.attach(img)
 
-
-@app.route('/map')
-def map():
-    
-    city = request.args.get('cities')
-    keyword = request.args.get('keyword')
-    radius = request.args.get('radius')
-    #################################開始在GOOGLE-MAP中搜尋######################################
-    ids = []
-    #定義輸出結果項目
-    myFields=['place_id','name','rating','user_ratings_total','formatted_address','international_phone_number','opening_hours','geometry']
-
-    results = []
-    geocode_result = gmaps.geocode(city)
-    loc = geocode_result[0]['geometry']['location']
-    query_result = gmaps.places_nearby(keyword=keyword,location=loc,radius=radius)
-    results.extend(query_result['results'])
-    while query_result.get('next_page_token'):
-        time.sleep(2)
-        query_result = gmaps.places_nearby(page_token=query_result['next_page_token'])
-        results.extend(query_result['results'])    
-    resultInfo = city+"為中心半徑"+str(radius)+"公尺內的"+keyword+"店家數量:"+str(len(results))+"間"
-   
-    if not results:
-        s="搜尋範圍內無符合條件店家,請擴大搜尋範圍重新搜尋"
-        return render_template('map.html', myResult=s, Resultlist = "")
-    else:
-        for place in results:
-            ids.append(place['place_id'])
-                
-        stores_info = []
-        # 去除重複id
-        ids = list(set(ids)) 
-        for id in ids:
-            stores_info.append(gmaps.place(place_id=id, fields= myFields, language='zh-TW')['result'])
-
-    #########################################對搜尋結果做處理############################################################
-        #去除搜尋結果中店名有汽車玻璃的店家
-        if (keyword == "玻璃"):
-            stores_info2 =[]
-            for i in range(0,len(results)):
-                if (not("汽車" in stores_info[i]['name'])):
-                    stores_info2.append(stores_info[i])
-                    output = pd.DataFrame.from_dict(stores_info2)
-        else:
-            output = pd.DataFrame.from_dict(stores_info)
-       
-        #對'user_ratings_total'進行降冪排序
-        output = output.sort_values(by='user_ratings_total', ascending=False)
-
-    #####################################################################################################      
-        #output.to_csv(keyword+'.csv',index=False, encoding='utf-8-sig')  
-   
-        d1 = output.to_dict('records')
-  
-        #print(d1)
-        return render_template('map.html', myResult=resultInfo, Resultlist = d1, Source=city)
+    # 連線至SMTP伺服器並發送郵件
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+       try:
+            smtp.ehlo()  # 驗證SMTP伺服器
+            smtp.starttls()
+            smtp.login('rueishian.chen@gmail.com', 'nmxmbawswwkfhwkk') # 登入寄件者gmail
+            smtp.sendmail('rueishian.chen@gmail.com', 'rueishian.chen@gmail.com', message.as_string())
+            myResult="規格上傳成功,我們將儘快與您聯繫,謝謝~"
+            print(myResult)
+            return render_template('index.html', myResult = myResult)
+       except Exception as e:
+            print("Error message: ", e)
+            myResult="規格上傳失敗,麻煩重新操作一次,謝謝~"
+            return render_template('index.html', myResult = myResult)
+        
